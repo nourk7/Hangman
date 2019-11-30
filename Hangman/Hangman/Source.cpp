9f6f6346db902b2f1565fdebc0b159631b22cb12
nourk7 <48683945+nourk7@users.noreply.github.com>
2019-11-30 19:21:45 +0200

    Add files via upload

---
+++ Hangman/Hangman/Source.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include<vector>
+#include<stack>
+using namespace std;
+
+struct word
+{
+	string name;
+	string meaning;
+};
+
+void category(char c)
+{
+	vector<word> data;
+	fstream file;
+	word temp;
+
+	switch (c)
+	{
+	case 'm':
+		file.open("m.txt");
+		break;
+
+	case's':
+		file.open("s.txt");
+		break;
+	default:
+		break;
+	}
+
+	
+	while (true)
+	{
+		getline(file, temp.name);
+		if (temp.name == "pause")
+		{
+			break;
+		}
+		getline(file, temp.meaning);
+		data.push_back(temp);
+	}
+	file.close();
+	for (int i = 0; i < data.size(); i++)
+	{
+		cout << "-----------------" << endl;
+		cout << data[i].name << endl;
+		cout << data[i].meaning << endl;
+	}
+
+}
+
+int main()
+{
+	cout << "enter category" << endl;
+	char ch;
+	cin >> ch;
+
+	category(ch);
+	
+		
+	
+	system("pause");
+}
